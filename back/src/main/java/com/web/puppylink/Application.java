9bcc2e1188f7baf2aec18994130fa8d99da01148
임지원 <jiwon4120@naver.com>
2023-02-03 16:17:03 +0900

    feat: s3  파일 업로드

--- back/src/main/java/com/web/puppylink/Application.java
+++ back/src/main/java/com/web/puppylink/Application.java
@@ -2,14 +2,22 @@ package com.web.puppylink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling		// 스케줄 태스크 실행하기 위해 추가해줘야함
 @SpringBootApplication
 public class Application {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+//		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(Application.class)
+        .properties(APPLICATION_LOCATIONS)
+        .run(args);
 	}
 
 }
--- back/src/main/java/com/web/puppylink/service/VolunteerService.java
+++ back/src/main/java/com/web/puppylink/service/VolunteerService.java
@@ -1,5 +1,9 @@
 package com.web.puppylink.service;
 
+import com.web.puppylink.model.Volunteer;
+
 public interface VolunteerService {
 
+	 Volunteer submitFile(String nickName, String imagePath);
+	 
 }
