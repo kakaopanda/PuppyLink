a2001e5e0455292a6f51c87eaf322674e1834ed5
appletail <csappletail@gmail.com>
2023-02-04 12:13:23 +0900

    feat: 봉사신청 작성 미완료시 경고 토스트
    - react-toastify 라이브러리로 작성 미완료시 경고 토스트
    	- 레이아웃 무너지는 문제는 에러메시지 span을 남겨놔서 그러함
    	- formState 중 isValid를 이용해 폼작성이 완료되었는지 아닌지
    	  구분하였음
    
    이슈: S08P12C107-186

--- front/src/pages/Volunteer/VolUserResiPage.tsx
+++ front/src/pages/Volunteer/VolUserResiPage.tsx
@@ -2,33 +2,34 @@ import { useForm } from 'react-hook-form';
 
 import { ToastContainer, toast } from 'react-toastify';
 
+import "react-toastify/dist/ReactToastify.css";
+
 import { DepDateFooter, DestFooter, FlightName, Foundation, DestTitle, DepDateTitle } from './Components'
 
-import 'react-toastify/dist/ReactToastify.css';
 import { cards, buttons } from '@/components';
 import VolUserStyle from '@/styles/pages/Volunteer/VolUserResiPage.module.css';
 
+
 function VolUserResi() {
-  const requiredData = {
-    dest: '미국 워싱턴',
-    depDate: '2023-02-04:00:00:00',
-    filghtName: 'korair',
-    businessNo: '1148209801',
-    termsService: true
+
+  const { control, handleSubmit, register, formState: { isValid } } = useForm()
+
+  const notify = () => {
+    if (!isValid) {
+      toast.error('모든 항목을 작성해주세요.', {
+        position: "bottom-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: 0,
+        theme: "colored",
+      })
+    }
   }
 
-  const { control, handleSubmit, register, formState: { errors } } = useForm()
 
-  const notify = () => toast.error('모든 항목을 작성해주세요.', {
-    position: "bottom-center",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: 0,
-    theme: "colored",
-  })
   return (
     <div>
       <button className={`${VolUserStyle.GoToMyRegi} bg-main-30`} type="button">
@@ -39,58 +40,43 @@ function VolUserResi() {
         <div className={VolUserStyle.FlightWrapper}>
           <cards.CardSm
             CardTitle={<DestTitle />}
-            CardFooter={<DestFooter
-              control={control}
-              name="dest"
+            CardFooter={<DestFooter control={control} name="dest"
               rules={{
-                validate: (v: string) => v != undefined,
+                validate: (v: string) => v != undefined && v != 'none',
               }}
             />} />
-          {errors.dest && <span>{`${errors.dest.message}`}</span>}
           <cards.CardSm
             CardTitle={<DepDateTitle />}
-            CardFooter={<DepDateFooter
-              control={control}
-              name="depDate"
+            CardFooter={<DepDateFooter control={control} name="depDate"
               rules={{
                 validate: (v: string) => v != undefined,
               }}
             />}
           />
-          {errors.depDate && <span>{`${errors.depDate.message}`}</span>}
         </div>
         <p className={VolUserStyle.SelectTitle}>항공사 선택하기</p>
         <div className={VolUserStyle.FlightSelect}>
-          <FlightName
-            control={control}
-            name="filghtName"
+          <FlightName control={control} name="filghtName"
             rules={{
               validate: (v: string) => v != undefined,
             }}
           />
-          {errors.filghtName && <span>{`${errors.filghtName.message}`}</span>}
         </div>
         <p className={VolUserStyle.SelectTitle}>단체 선택하기</p>
         <div className={VolUserStyle.GroupSelect}>
-          <Foundation
-            control={control}
-            name="businessNo"
+          <Foundation control={control} name="businessNo"
             rules={{
               validate: (v: string) => v != undefined,
             }}
           />
-          {errors.businessNo && <span>{`${errors.businessNo.message}`}</span>}
         </div>
         <div className={VolUserStyle.Terms}>
-          <input
-            id="termsService"
-            type="checkbox"
+          <input id="termsService" type="checkbox"
             {...register("termsService", {
               validate: v => v
             })}
           />
           <label htmlFor="termsService">주의사항을 읽었고 개인정보 제공에 동의합니다.</label>
-          {errors.termsService && <span>{`${errors.termsService.message}`}</span>}
         </div>
         <buttons.BtnLg BtnValue="신청하기" onClick={notify} />
         <ToastContainer />
