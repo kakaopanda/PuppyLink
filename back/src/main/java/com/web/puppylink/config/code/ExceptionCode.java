9c751594ec7e8adf455653e038a1d5472a6340c8
강주형 <kakao_panda@naver.com>
2023-02-08 16:48:24 +0900

    refactor: 메소드 리턴 타입(BasicResponseDto) 통일

--- back/src/main/java/com/web/puppylink/config/code/ExceptionCode.java
+++ back/src/main/java/com/web/puppylink/config/code/ExceptionCode.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ExceptionCode {
 
-
-
     EXCEPTION_SIGNUP(5001, "회원가입에 실패하였습니다."),
     EXCEPTION_DATA(5002,"DB 관련 오류가 발생하였습니다."),
     EXCEPTION_MAIL(5003,"메일 전송하면서 오류가 발생하였습니다.");
