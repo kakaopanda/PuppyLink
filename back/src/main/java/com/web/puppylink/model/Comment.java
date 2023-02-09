d2aeb570ffe8dc8bd0110c80e5a5756e8a2c2dea
강주형 <kakao_panda@naver.com>
2023-02-09 17:18:22 +0900

    feat: 게시판 댓글 조회,삽입,수정,삭제 구현

--- back/src/main/java/com/web/puppylink/model/Comment.java
+++ back/src/main/java/com/web/puppylink/model/Comment.java
@@ -2,19 +2,23 @@ package com.web.puppylink.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 import java.util.Date;
 
 @Entity
 @Table(name = "comments")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Comments {
+public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
