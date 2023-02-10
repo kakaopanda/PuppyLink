b32594072c7905e93987542a1b1f0571f98a6d2d
강주형 <kakao_panda@naver.com>
2023-02-10 10:54:53 +0900

    feat: OCR 반환 형태 및 항공권 제출 형태 변경

--- back/src/main/java/com/web/puppylink/service/FlightTicketService.java
+++ back/src/main/java/com/web/puppylink/service/FlightTicketService.java
@@ -4,4 +4,7 @@ import com.web.puppylink.model.FlightTicket;
 
 public interface FlightTicketService {
 	FlightTicket submit(FlightTicket flightTicket);
+	
+	// ※ Google Vision Api를 통해, 업로드된 항공권 이미지에 대한 OCR 결과를 반환 및 저장
+	FlightTicket ocr(int volunteerNo);
 }
