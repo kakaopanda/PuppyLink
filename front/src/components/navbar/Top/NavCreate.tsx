2e0c7b5c7335011c1d152c5fc074eb8b834cee25
정하림 <bl7655@naver.com>
2023-02-15 11:20:08 +0900

    feat: 게시판 리뷰 작성, 댓글 작성

--- front/src/components/navbar/Top/NavCreate.tsx
+++ front/src/components/navbar/Top/NavCreate.tsx
@@ -1,17 +1,33 @@
 import { FiChevronLeft } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom'
 
 import NavStyle from './NavTop.module.css';
 
-function NavCreate() {
+interface Review {
+  contents: string
+  subject: string
+  pictureURL: string
+}
+
+interface ReviewProps {
+  onClick: React.MouseEventHandler
+}
+
+
+function NavCreate({ onClick }: ReviewProps): JSX.Element {
+  const navigate = useNavigate()
+  const goToBack = () => navigate(-1)
   return (
     <div className={NavStyle.NavBg}>
-      <div className={NavStyle.Left}>
+      <div className={NavStyle.Left} onClick={goToBack}>
         <FiChevronLeft color="white" />
         <div> 이전 </div>
       </div>
       <div className={NavStyle.Center}></div>
 
-      <div className={NavStyle.Right}>작성</div>
+      <div className={NavStyle.Right} onClick={onClick} >
+        작성
+      </div>
     </div>
   );
 }
